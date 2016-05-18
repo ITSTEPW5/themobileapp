2e0213f4ae03894b45511624e17761aca92c4d9b
Dmytro Krytovych <dima.krytovych@gmail.com>
2016-05-18 19:37:15 +0300

    Create structure. Slyce images

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+	require('layout/header.php');
+?>
+
+
+<section class="promo">
+	
+	<div class="promo__inner">
+		
+	</div>
+
+</section>
+
+<section style="height: 500px;"></section>
+
+
+<?php 
+	require('layout/footer.php');
+?>
